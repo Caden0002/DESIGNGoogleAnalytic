aa82e9e7d356dcbb81150dd7af96ead430d643ba
caden <chen.qixuan97@gmail.com>
2024-04-17 23:17:18 +0800

    “page_view”

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-27JWP98W69");
+
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
